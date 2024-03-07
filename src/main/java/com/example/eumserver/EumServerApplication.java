7c4674ae0523fcb92de46164e321a6c5264927f4
mclub4 <mclub4@kookmin.ac.kr>
2024-03-07 16:50:00 +0900

    first commit

---
+++ src/main/java/com/example/eumserver/EumServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.eumserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EumServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EumServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eumserver/EumServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eumserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EumServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
