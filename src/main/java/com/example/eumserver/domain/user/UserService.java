5d2fd3e4a778039e451b6616b8e6c38b08a0d644
sukjuhong <ehensnfl@gmail.com>
2024-03-20 14:57:16 +0900

    fix: 유저 조회 시 필터를 다시 도는 현상 (#1)
    UserService에서 UsernameNotFoundException을 호출해서 발생하는 현상

--- src/main/java/com/example/eumserver/domain/user/UserService.java
+++ src/main/java/com/example/eumserver/domain/user/UserService.java
@@ -1,7 +1,6 @@
 package com.example.eumserver.domain.user;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,7 +12,7 @@ public class UserService {
     private final UserRepository userRepository;
 
     public User findByEmail(String email) {
-        return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found."));
+        return userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("User not found."));
     }
 
 }
