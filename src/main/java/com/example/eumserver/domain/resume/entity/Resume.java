08fb46decce616c337f9ff409cb0f83ce94a8965
mclub4 <mclub4@kookmin.ac.kr>
2024-04-01 20:04:27 +0900

    feat: #10/이력서 경력 추가

--- src/main/java/com/example/eumserver/domain/resume/entity/Resume.java
+++ src/main/java/com/example/eumserver/domain/resume/entity/Resume.java
@@ -1,6 +1,7 @@
 package com.example.eumserver.domain.resume.entity;
 
 import com.example.eumserver.domain.user.User;
+import com.example.eumserver.global.entity.TimeStamp;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
@@ -56,13 +57,7 @@ public class Resume {
     @Column(columnDefinition = "TEXT", nullable = false)
     private String introduction;
 
-    @CreationTimestamp
-    @Column(name = "create_date", nullable = false, updatable = false)
-    private LocalDateTime createDate;
-
-    @UpdateTimestamp
-    @Column(name = "update_at", nullable = false)
-    private LocalDateTime updateAt;
-
+    @Embedded
+    private TimeStamp timeStamp;
 
 }
--- src/main/java/com/example/eumserver/domain/resume/entity/ResumeCareer.java
+++ src/main/java/com/example/eumserver/domain/resume/entity/ResumeCareer.java
@@ -1,9 +1,14 @@
 package com.example.eumserver.domain.resume.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDate;
+
+/**
+ * 이력에서 자신의 경력을 담는 Entity
+ * TODO : 회사에서 이용했던 기술들을 적는 technologiesUsed를 구현해야됨 (별도 테이블 생성?)
+ */
 @Entity
 @Table(name = "resume_careers")
 @Getter
@@ -11,5 +16,35 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ResumeCareer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "resume_career_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "resume_id", nullable = false)
+    private Resume resume;
+
+    @Column(name = "company_name", nullable = false, length = 15)
+    private String companyName;
+
+    @Column(name = "company_role", nullable = false, length = 20)
+    private String companyRole;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    @Builder.Default
+    private String technologiesUsed = "Spring 장인";
+
+    @Column(name = "company_website_url", length = 255)
+    private String companyWebsiteUrl;
+
+    @Column(columnDefinition = "TEXT")
+    private String achievement;
+
 
 }
