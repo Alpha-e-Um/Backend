51d12dbf327887253626f80e2c23e5021e383b6d
mclub4 <mclub4@kookmin.ac.kr>
2024-04-09 03:06:19 +0900

    fix: #21/Auth Controller Method 변경

--- src/main/java/com/example/eumserver/domain/oauth2/AuthController.java
+++ src/main/java/com/example/eumserver/domain/oauth2/AuthController.java
@@ -7,7 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +19,7 @@ public class AuthController {
 
     private final AuthService authService;
 
-    @GetMapping("/reissue")
+    @PostMapping("/reissue")
     public ResponseEntity<TokenResponse> reissue(HttpServletRequest request, HttpServletResponse response) {
         TokenResponse tokenResponse = authService.reissueToken(request, response);
         return ResponseEntity
