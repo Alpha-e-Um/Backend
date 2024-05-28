ba156f1fe9af40b96912795c8134808348c4e095
sukjuhong <ehensnfl@gmail.com>
2024-05-28 14:47:34 +0900

    feat: 이벤트 비동기 처리 (#61)

--- business/src/main/java/com/example/eumserver/domain/notification/listener/NotificationListener.java
+++ business/src/main/java/com/example/eumserver/domain/notification/listener/NotificationListener.java
@@ -4,6 +4,7 @@ import com.example.eumserver.domain.notification.domain.NotificationEvent;
 import com.example.eumserver.domain.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionalEventListener;
 
@@ -13,6 +14,7 @@ public class NotificationListener {
 
   private final NotificationService notificationService;
 
+  @Async
   @TransactionalEventListener
   public void saveNotification(NotificationEvent notificationEvent) {
     notificationService.saveWithConversion(notificationEvent);
