e9546f220b6e90208d389eb406b5b105102b4ae7
sukjuhong <ehensnfl@gmail.com>
2024-05-29 19:21:10 +0900

    feat: 팀 공고 Response 필드 추가 (#94)

--- business/src/main/java/com/example/eumserver/domain/announcement/team/dto/TeamAnnouncementResponse.java
+++ business/src/main/java/com/example/eumserver/domain/announcement/team/dto/TeamAnnouncementResponse.java
@@ -1,7 +1,15 @@
 package com.example.eumserver.domain.announcement.team.dto;
 
+import java.time.LocalDateTime;
+
 public record TeamAnnouncementResponse(
+        Long id,
         String title,
-        String description
+        String description,
+        String region,
+        LocalDateTime createDate,
+
+        Long teamId,
+        String teamLogo
 ) {
 }
