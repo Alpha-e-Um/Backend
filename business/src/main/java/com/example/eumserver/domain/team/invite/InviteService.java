22cf6b212c45da7a562a638f9f144152f64fbde3
sukjuhong <ehensnfl@gmail.com>
2024-06-01 16:08:30 +0900

    feat: 팀 응답에 소유 여부 포함하기 (#102)

--- business/src/main/java/com/example/eumserver/domain/team/invite/InviteService.java
+++ business/src/main/java/com/example/eumserver/domain/team/invite/InviteService.java
@@ -1,7 +1,7 @@
 package com.example.eumserver.domain.team.invite;
 
 import com.example.eumserver.domain.team.Team;
-import com.example.eumserver.domain.team.TeamRepository;
+import com.example.eumserver.domain.team.repository.TeamRepository;
 import com.example.eumserver.domain.team.participant.Participant;
 import com.example.eumserver.domain.team.participant.ParticipantRepository;
 import com.example.eumserver.domain.user.User;
