c08c255bb9495fce91c85bf3dca837561ba52fd6
sukjuhong <ehensnfl@gmail.com>
2024-06-02 14:05:39 +0900

    feat: 팀 참가자 정보 조회 구현 (#111)

--- business/src/main/java/com/example/eumserver/domain/team/TeamController.java
+++ business/src/main/java/com/example/eumserver/domain/team/TeamController.java
@@ -2,6 +2,8 @@ package com.example.eumserver.domain.team;
 
 import com.example.eumserver.domain.jwt.PrincipalDetails;
 import com.example.eumserver.domain.team.dto.TeamRequest;
+import com.example.eumserver.domain.team.participant.ParticipantService;
+import com.example.eumserver.domain.user.dto.UserResponse;
 import com.example.eumserver.global.annotation.MaxFileSize;
 import com.example.eumserver.global.dto.ApiResult;
 import lombok.RequiredArgsConstructor;
@@ -12,12 +14,15 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/team")
 @RequiredArgsConstructor
 public class TeamController {
 
     private final TeamService teamService;
+    private final ParticipantService participantService;
 
     private static final int MAX_TEAM_LOGO_SIZE = 3 * 1024 * 1024; // 3MB
 
@@ -53,6 +58,14 @@ public class TeamController {
         return ResponseEntity
                 .status(HttpStatus.ACCEPTED)
                 .body(new ApiResult<>("팀 삭제 성공"));
+    }
+
+    @GetMapping("/{teamId}/participant")
+    public ResponseEntity<ApiResult<List<UserResponse>>> getAllParticipant(
+            @PathVariable long teamId
+    ) {
+        List<UserResponse> userResponses = participantService.getAllParticipantByTeamId(teamId);
+        return ResponseEntity.ok(new ApiResult<>("팀 참가자 조회 성공", userResponses));
     }
 
 }
