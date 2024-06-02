be8abbc609d7a34d9e25d8b14795025cc0700df1
mclub4 <mclub4@kookmin.ac.kr>
2024-06-03 00:48:13 +0900

    chore: #107-팀원 구하기 공고 Swagger 작성

--- business/src/main/java/com/example/eumserver/domain/announcement/team/controller/TeamAnnouncementController.java
+++ business/src/main/java/com/example/eumserver/domain/announcement/team/controller/TeamAnnouncementController.java
@@ -3,6 +3,7 @@ package com.example.eumserver.domain.announcement.team.controller;
 import com.example.eumserver.domain.announcement.team.dto.*;
 import com.example.eumserver.domain.announcement.team.service.TeamAnnouncementService;
 import com.example.eumserver.global.dto.ApiResult;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +22,7 @@ public class TeamAnnouncementController {
     private final TeamAnnouncementService announcementService;
 
     @PostMapping("")
+    @Operation(summary = "팀원 구하기 공고 올리기", description = "팀원 구하기 공고를 생성합니다.")
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<ApiResult<TeamAnnouncementResponse>> createAnnouncement(
             @RequestBody TeamAnnouncementRequest announcementRequest
@@ -40,6 +42,7 @@ public class TeamAnnouncementController {
      * @return 페이징이 적용된 팀 공고 리스트
      */
     @GetMapping("")
+    @Operation(summary = "팀원 구하기 공고를 필터에 따라 조회", description = "만료 여부, 검색 필터, 글 목록 필터 등에 따라 페이지네이션으로 보여줍니다.")
     public ResponseEntity<ApiResult<TeamAnnouncementResponseWrapper>> getAnnouncements(
             TeamAnnouncementFilter filter
     ) {
@@ -49,6 +52,7 @@ public class TeamAnnouncementController {
     }
 
     @GetMapping("/{announcementId}")
+    @Operation(summary = "팀원 구하기 공고 세부 조회", description = "팀원 구하기 공고의 세부 사항을 보여줍니다.")
     public ResponseEntity<ApiResult<TeamAnnouncementDetailResponse>> getAnnouncement(
             @RequestHeader(name = "Authorization", required = false) String authorization,
             @PathVariable(name = "announcementId") Long announcementId) {
@@ -57,7 +61,9 @@ public class TeamAnnouncementController {
                 .ok(new ApiResult<>("팀 공고 조회 성공", announcement));
     }
 
+    //TODO : 팀원 구하기 공고 수정 및 삭제가 Token을 받지 않고 진행되는데, 왜 이렇게 한 것인지 궁금합니다.
     @PutMapping("/{announcementId}")
+    @Operation(summary = "팀원 구하기 공고 수정", description = "팀원 구하기 공고의 세부 사항을 보여줍니다.")
     public ResponseEntity<ApiResult<?>> updateAnnouncement(
             @PathVariable(name = "announcementId") Long announcementId,
             @RequestBody TeamAnnouncementUpdateRequest announcementUpdateRequest
@@ -68,6 +74,7 @@ public class TeamAnnouncementController {
     }
 
     @DeleteMapping("/{announcementId}")
+    @Operation(summary = "팀원 구하기 공고 삭제", description = "팀원 구하기 공고를 삭제합니다.")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseEntity<ApiResult<?>> deleteAnnouncement(
             @PathVariable(name = "announcementId") Long announcementId
--- business/src/main/java/com/example/eumserver/domain/post/strategy/SortingStrategyFactory.java
+++ business/src/main/java/com/example/eumserver/domain/post/strategy/SortingStrategyFactory.java
@@ -1,6 +1,8 @@
 package com.example.eumserver.domain.post.strategy;
 
 import com.example.eumserver.domain.post.PostSortingOption;
+import com.example.eumserver.global.error.exception.CustomException;
+import com.example.eumserver.global.error.exception.ErrorCode;
 import com.querydsl.core.types.dsl.DateTimePath;
 import com.querydsl.core.types.dsl.EntityPathBase;
 import com.querydsl.core.types.dsl.NumberPath;
@@ -8,6 +10,9 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
+/**
+ * 글 목록 필터를 Strategy전략을 활용하여, Factory로 분배해줍니다.
+ */
 @Component
 public class SortingStrategyFactory {
 
@@ -21,7 +26,7 @@ public class SortingStrategyFactory {
             case POPULAR:
                 return new PopularSortingStrategy<>(viewsPath, createdAtpath);
             default:
-                throw new IllegalArgumentException("Unknown sorting option: " + option);
+                throw new CustomException("잘못된 글 목록 옵션", ErrorCode.INVALID_INPUT_VALUE);
         }
     }
 }
